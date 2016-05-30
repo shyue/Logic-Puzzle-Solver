354db684df28c3ca178fcbb6bdf6fdaa99a20dae
JoshYuan <j05hy4tw@gmail.com>
2016-05-30 12:58:38 -0400

    Update Reader.lisp

--- Reader.lisp
+++ Reader.lisp
@@ -1,12 +1,13 @@
 ;Reads file
 ;filepath = "/home/330542/Documents/11th grade/AI/LogicObjects.txt"
 (defun reader (filepath)
-(setf input (open filepath :if-does-not-exist nil))
-(setf features nil)
+  (setf input (open filepath :if-does-not-exist nil))
+  (setf features nil)
+  (setf featureLists nil)
   (when input
     (setf features (read input nil))
-    (format t "~a~%" features)
+    (print features)
     (dotimes (counter (length features))
-      (set (nth counter features) (read input nil))
-      (print (eval (nth counter features)))))
-    (close input))
+      (setf featureLists (append featureLists (list (read input nil))))))
+  (print featureLists)
+  (close input))
